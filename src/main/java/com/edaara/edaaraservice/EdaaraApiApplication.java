1d537ddba39bc8c078731e827fbd09aacbac5795
Moussa Niasse <niassemoussa94@gmail.com>
2025-05-19 19:35:51 +0000

    Initialisation du repos

---
+++ src/main/java/com/edaara/edaaraservice/EdaaraApiApplication.java
@@ -0,0 +1,13 @@
+package com.edaara.edaaraservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdaaraApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdaaraApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edaara/edaaraservice/EdaaraApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edaara.edaaraservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdaaraApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
